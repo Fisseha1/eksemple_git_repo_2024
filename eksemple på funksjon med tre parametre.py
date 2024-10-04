3fa59c4711756224f3617b1bb0a70070a004ce2d
Fisseha1 <fissehafish@yahoo.com>
2024-10-04 20:27:27 +0200

    Update eksemple på funksjon med tre parametre.py

--- eksemple på funksjon med tre parametre.py
+++ eksemple på funksjon med tre parametre.py
@@ -5,17 +5,17 @@ def stjern_firkant(hoyde, bredde, tegn="*"):
         for x in range(bredde):
             print(tegn,end="")# end="" for å ikke få newline på slutten
         print()
-    hoyde= hoyde +3
+    hoyde= hoyde +6
     print("økt hoyde inni funksjon", hoyde)
  
   
  
  # kode som demonstrerer funksjon
-stjern_firkant(4,3,"#")
+stjern_firkant(6,5,"#")
 hoyde=int(input("skriv inn høyde:"))
 bredde= int(input(" skriv inn bredde"))
 stjern_firkant(hoyde, bredde)
 #print("høyde til slut", hoyde)
-stjern_firkant(tegn="*", bredde =8, hoyde =3)
-stjern_firkant(tegn="0",bredde=4, hoyde=7)
-stjern_firkant(tegn="#",bredde=6, hoyde=8)
+stjern_firkant(tegn="0", bredde =8, hoyde =3)
+stjern_firkant(tegn="+",bredde=4, hoyde=7)
+stjern_firkant(tegn="*",bredde=2, hoyde=4)
