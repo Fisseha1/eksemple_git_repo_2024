850992bdd689110a32556aac6263fbe2b8a8311d
Fisseha1 <fissehafish@yahoo.com>
2024-10-04 22:14:27 +0200

    la inn for loop

---
+++ turtle1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 17:23:18 2024
+
+@author: Fisseha Araya
+"""
+
+import turtle
+import math
+
+turtle.pencolor("orange")
+turtle.pensize(5)
+turtle.fillcolor("green")
+turtle.begin_fill()
+for i in range(3):
+    turtle.forward(100)
+    turtle.right(120)
+
+turtle.end_fill()# vi luke med den som vi har åpnet fillcolor, uten den funger det ikke fillcolor
+turtle.done()
