d1e18befe350d94045347d8f6159e4c1d1ca7184
Fisseha1 <fissehafish@yahoo.com>
2024-10-05 12:23:24 +0200

    fjernet trekant til 3trekant

--- turtle1.py
+++ turtle1.py
@@ -8,21 +8,21 @@ Created on Wed Sep  4 17:23:18 2024
 import turtle
 import math
 
-def tegn_trekant(pennsize=9,penncolor="orange",fill="green"):
+def tegn_3kant(pennsize=9,penncolor="orange",fill="green"):
     turtle.pencolor(penncolor)
     turtle.pensize(pennsize)
     turtle.fillcolor(fill)
     turtle.begin_fill()
         
-    turtle.forward(100)
+    turtle.forward(130)
     turtle.right(120)
-    turtle.forward(100)
+    turtle.forward(130)
     turtle.right(120)
-    turtle.forward(100)
+    turtle.forward(130)
     turtle.end_fill()
-tegn_trekant()
+tegn_3kant()
 turtle.penup()
-turtle.goto(-100,100)
+turtle.goto(-130,130)
 turtle.pendown()
-tegn_trekant(3,"black","blue")
+tegn_3kant(3,"red","blue")
 turtle.done()
